5f8c931240299a2a81e20c7742e5b807a3e3cf7f
Crowdbotics <team@crowdbotics.com>
2020-11-03 16:04:39 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/mysoccer_22259/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mysoccer_22259;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "mysoccer_22259";
+    }
+}
